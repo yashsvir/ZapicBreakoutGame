41d274bc61edbd9ab68989f926db8132f34f6973
Rawal <yashsvir@gmail.com>
2017-10-11 01:41:20 -0600

    Adding inital project

---
+++ Assets/Zapic/Examples/ExampleStartup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZapicSDK;
+
+public class ExampleStartup : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Zapic.Connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
