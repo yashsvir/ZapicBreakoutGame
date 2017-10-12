e1a95789f6b80bb3a6a1df9cd52ca76c435edf2e
Rawal <yashsvir@gmail.com>
2017-10-11 23:02:09 -0600

    White Box Build.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour {
+
+    private float _initalVelocity = 300f;
+    private Rigidbody _rb;
+    private float _initialBallAngle;
+    void Awake()
+    {
+        StartCoroutine(FireBall());
+        _rb = GetComponent<Rigidbody>();
+        _initialBallAngle = UnityEngine.Random.Range(0, 360);
+    }
+
+    private IEnumerator FireBall()
+    {
+        yield return new WaitForSeconds(2);
+
+        _rb.isKinematic = false;
+        _rb.AddForce(new Vector3(_initalVelocity*Mathf.Sin(_initialBallAngle *Mathf.Deg2Rad), _initalVelocity * Mathf.Cos(_initialBallAngle * Mathf.Deg2Rad), 0));
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Brick")
+        {
+            Destroy(col.gameObject);
+        }
+        
+
+    }
+
+    /*void Update() {
+        Debug.Log(_rb.velocity.magnitude);
+    }*/
+}
